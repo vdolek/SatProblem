32ae20c0827fa4c64c712c3f425c2cdf2106c6f9
Martin Volek <martin@vdolek.cz>
2016-01-24 15:55:08 +0100

    BrutteForceTest

---
+++ src/SatProblem/Tests/BruteForceSolverTests.cs
@@ -0,0 +1,78 @@
+﻿using Cz.Volek.CVUT.FIT.MIPAA.SatProblem.Formulas;
+using Cz.Volek.CVUT.FIT.MIPAA.SatProblem.Models;
+using Cz.Volek.CVUT.FIT.MIPAA.SatProblem.Solvers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cz.Volek.CVUT.FIT.MIPAA.SatProblem.Tests
+{
+    [TestClass]
+    public class BruteForceSolverTests
+    {
+        [TestMethod]
+        public void TestLogicalSum()
+        {
+            // n = 4 
+            // F = (x0 + x2' + x3).(x0' + x1 + x2').(x2 + x3).(x0 + x1 + x2' + x3').(x1' + x2).(x2' + x3') 
+            // W = (2, 4, 1, 6)
+
+            // expected solution: {1, 0, 0, 1}, weight: 8
+
+            var formula = new LogicalProductFormula(new IFormula[]
+            {
+                // (x0 + x2' + x3)
+                new LogicalSumFormula(new IFormula[]
+                {
+                    new LiteralFormula(0),
+                    new NegateFormula(new LiteralFormula(2)),
+                    new LiteralFormula(3),
+                }),
+                // (x0' + x1 + x2')
+                new LogicalSumFormula(new IFormula[]
+                {
+                    new NegateFormula(new LiteralFormula(0)),
+                    new LiteralFormula(1),
+                    new NegateFormula(new LiteralFormula(2)),
+                }),
+                // (x2 + x3)
+                new LogicalSumFormula(new IFormula[]
+                {
+                    new LiteralFormula(2),
+                    new LiteralFormula(3)
+                }),
+                // (x0 + x1 + x2' + x3')
+                new LogicalSumFormula(new IFormula[]
+                {
+                    new LiteralFormula(0),
+                    new LiteralFormula(1),
+                    new NegateFormula(new LiteralFormula(2)),
+                    new NegateFormula(new LiteralFormula(3)),
+                }),
+                // (x1' + x2)
+                new LogicalSumFormula(new IFormula[]
+                {
+                    new NegateFormula(new LiteralFormula(1)),
+                    new LiteralFormula(2)
+                }),
+                // (x2' + x3')
+                new LogicalSumFormula(new IFormula[]
+                {
+                    new NegateFormula(new LiteralFormula(2)),
+                    new NegateFormula(new LiteralFormula(3))
+                })
+            });
+
+            var instance = new Instance
+            {
+                Formula = formula,
+                Weights = new [] { 2, 4, 1, 6 }
+            };
+
+            var solver = new BrutteForceSolver();
+            var res = solver.Solve(instance);
+
+            var expectedConfiguration = 1L | (1L << 3);
+            Assert.AreEqual(expectedConfiguration, res.Configuration);
+            Assert.AreEqual(8, res.Weight);
+        }
+    }
+}
